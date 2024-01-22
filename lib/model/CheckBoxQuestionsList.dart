1791396be4866de1e559a55051b8aaf319f2b7ff
ankur <ankurmaurya.sms@gmail.com>
2024-01-21 20:49:21 -0800

    git initialized...

--- lib/model/CheckBoxQuestionsList.dart
+++ lib/model/CheckBoxQuestionsList.dart
@@ -18,7 +18,7 @@ List<QuestionsListDataModel> questionsList = [
   QuestionsListDataModel(
     data: "Add the patient/Choose the patient (if already add).",
     fullData: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-    enabled: true,
+    enabled: false,
   ),
   QuestionsListDataModel(
     data: "Provide personal information including name and address.",
@@ -33,6 +33,6 @@ List<QuestionsListDataModel> questionsList = [
   QuestionsListDataModel(
     data: "Pay for the test including a secure online payment method.",
     fullData: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-    enabled: true,
+    enabled: false,
   ),
 ];
